07d3cf88b30dc10897a939c328b862a6540fe991
mislamfullstackdev <mislam.pasona@gmail.com>
2024-11-18 12:55:25 +0900

    DjangoVueEcommerce basic apps installed

---
+++ apps/core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from apps.store.models import Product
+
+# Create your views here.
+def frontpage(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'frontpage.html', context)
+
+def contact(request):
+    return render(request, 'contact.html')
