ba0424cf30d71b74b8680da4de1aaf31af2d4acd
mislamfullstackdev <mislam.pasona@gmail.com>
2024-11-18 20:53:57 +0900

    completed product details and category details page

--- apps/store/models.py
+++ apps/store/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -20,7 +20,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
-    slug = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
 
     def __str__(self):
         return self.title
