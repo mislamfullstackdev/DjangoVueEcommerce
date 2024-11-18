0a63b5a0ff5e265962bff9450c512bfa5e2ebba5
mislamfullstackdev <mislam.pasona@gmail.com>
2024-11-19 01:14:34 +0900

    Added orderin category, featured product, added apps for cart etc

--- apps/store/models.py
+++ apps/store/models.py
@@ -4,9 +4,11 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
+    ordering = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
+        ordering = ('ordering', )
 
     def __str__(self):
         return self.title
@@ -21,6 +23,11 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
     slug = models.SlugField(max_length=255)
+    is_featured = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_added',)
 
     def __str__(self):
         return self.title
--- saulgadgets/urls.py
+++ saulgadgets/urls.py
@@ -16,14 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from apps.core.views import frontpage, contact
+from apps.core.views import frontpage, contact, about
 from apps.store.views import product_detail, category_detail
+from apps.cart.views import cart
 
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('contact/', contact, name='contact'),
+    path('about/', about, name='about'),
+    path('cart/', cart, name='cart'),
+    path('admin/', admin.site.urls),
     path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail'),
     path('<slug:slug>/', category_detail, name='category_detail'),
-    path('admin/', admin.site.urls),
 ]
